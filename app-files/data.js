60c5657314abd8baadbbdd7544aa4efd146d26de
hyeyoung91 <hyeyoungjung91@gmail.com>
2024-10-16 11:20:14 +0900

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,201 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "파노라마3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.42718595926183767,
+          "pitch": 0.046450175509253455,
+          "rotation": 6.283185307179586,
+          "target": "1-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.60324647393292,
+          "pitch": -0.37564496478240095,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">상가 전면</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">@@@ 상가의 정면 디자인</font></font><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">심플,노멀함을 강조한 파사드</font></font></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-5",
+      "name": "파노라마5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13453962620223336,
+          "pitch": 0.03161623449661377,
+          "rotation": 0,
+          "target": "4-8"
+        },
+        {
+          "yaw": -1.4508767430449314,
+          "pitch": -0.06308226744284084,
+          "rotation": 0,
+          "target": "1-5"
+        },
+        {
+          "yaw": 1.9506593488475046,
+          "pitch": -0.0988023013693109,
+          "rotation": 0,
+          "target": "3-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2107500533400035,
+          "pitch": -0.20788892617543553,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">옥외 지역&nbsp;</font></font></font></font></font></font><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">접근 A</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">A 방향 접근로</font></font></font></font>"
+        },
+        {
+          "yaw": 2.0459566342197064,
+          "pitch": -0.19199714067888607,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">옥외 접근 경로 B</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">B 방향 접근로</font></font>"
+        },
+        {
+          "yaw": 0.37641642127280406,
+          "pitch": -0.17576684466302694,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">가게 진입로</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">가게 내부로 들어가는 진입로</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-6",
+      "name": "파노라마6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-7",
+      "name": "파노라마7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-8",
+      "name": "파노라마8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-9",
+      "name": "파노라마9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
